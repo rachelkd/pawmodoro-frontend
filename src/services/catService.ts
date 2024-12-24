e71ea81ecfc32bca0ec430334ff1ffa0aae10a07
rachelkd <rachel05deng@gmail.com>
2024-12-23 18:03:07 -0800

    Refactor cat fetching API endpoint for improved clarity
    - Updated the API endpoint in fetchUserCats function from 'api/cats/user/${username}' to 'api/cats/${username}'.
    - This change aligns with recent API updates, ensuring a more straightforward approach to fetching user-specific cat data.

--- src/services/catService.ts
+++ src/services/catService.ts
@@ -5,7 +5,7 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const fetchUserCats = async (username: string): Promise<CatsResponse> => {
     console.log('Fetching cats for: ', username);
-    const response = await fetch(`${API_URL}/api/cats/user/${username}`, {
+    const response = await fetch(`${API_URL}/api/cats/${username}`, {
         headers: {
             'Authorization': `Bearer ${localStorage.getItem(STORAGE_KEYS.ACCESS_TOKEN)}`,
         },
